ddaa978cf245db382e36a92d6dd49927621a6d6d
jesse <jsoldat@hotmail.com>
2017-06-16 09:23:04 -0400

    refactored to pass the event type

--- src/app/event-bus-experiments/event-bus-experiments.component.ts
+++ src/app/event-bus-experiments/event-bus-experiments.component.ts
@@ -1,6 +1,6 @@
 import { testLessons } from './../shared/model/test-lessons';
 import { Component, OnInit } from '@angular/core';
-import { globalEventBus} from './event-bus'; 
+import { globalEventBus, LESSONS_LIST_AVAILABLE} from './event-bus'; 
 
 
 @Component({
@@ -14,7 +14,8 @@ export class EventBusExperimentsComponent implements OnInit {
 
   ngOnInit() {
     console.log('Top level component broadcasted all lesssons');
-    globalEventBus.notifyObservers(testLessons);
+    globalEventBus.notifyObservers(LESSONS_LIST_AVAILABLE, 
+    testLessons.slice(0));
   }
 
 }
--- src/app/lessons-list/lessons-list.component.ts
+++ src/app/lessons-list/lessons-list.component.ts
@@ -1,9 +1,7 @@
-import { Lesson } from './../shared/model/lesson';
-import { globalEventBus, Observer } from './../event-bus-experiments/event-bus';
-
 import { Component, OnInit } from '@angular/core';
-import * as _ from 'lodash';
 
+import { Lesson } from './../shared/model/lesson';
+import { globalEventBus, Observer, LESSONS_LIST_AVAILABLE } from './../event-bus-experiments/event-bus';
 
 @Component({
   selector: 'lessons-list',
@@ -11,19 +9,19 @@ import * as _ from 'lodash';
   styleUrls: ['./lessons-list.component.css']
 })
 export class LessonsListComponent implements OnInit, Observer {
-  lessons: Lesson[] = []
+  lessons: Lesson[] = [];
+
   constructor() {
     console.log('lesson list is registered as a listener ');
-   globalEventBus.registerObserver(this);
+    globalEventBus.registerObserver(LESSONS_LIST_AVAILABLE, this);
    }
 
   ngOnInit() {
-   
-                 
+              
   }
+
   notify(data: Lesson[]) {
     console.log('lessons list component received data');
-    
     this.lessons = data;
   }
 
